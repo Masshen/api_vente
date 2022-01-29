d197ebfef16cdf50a14e41ef376cf78f5956927c
jacquesTshingambu <jacquestshingambu27@gmail.com>
2022-01-29 16:46:06 +0100

    traitement du controlleur pour le modele user

--- app/Http/Controllers/Api/Usercontroller.php
+++ app/Http/Controllers/Api/Usercontroller.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;//pour importer les validation
+use Illuminate\Validation\Rule;
 
 class Usercontroller extends Controller
 {
@@ -14,7 +17,8 @@ class Usercontroller extends Controller
      */
     public function index()
     {
-        //
+        $list=User::all();
+        return response()->json($list);
     }
 
     /**
@@ -25,7 +29,24 @@ class Usercontroller extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validation=Validator::make($request->all(),[
+            'email'=>'required|unique:users,email|email',
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'password'=>'required',
+            'type'=>['required',Rule::in(['agent','customer'])]//rule definie la regle
+        ]);
+           if($validation->fails()){
+            return response()->json($validation->getMessageBag(),422);// fails verifier s'il ya echec
+        }
+        $model = new User();
+        $model->first_name=$request->first_name;
+        $model->last_name=$request->last_name;
+        $model->email=$request->email;
+        $model->type=$request->type;
+        $model->password=$request->password;
+        $model->save();// methode pour enregistrer
+        return response()->json($model);
     }
 
     /**
@@ -36,7 +57,11 @@ class Usercontroller extends Controller
      */
     public function show($id)
     {
-        //
+        $model=User::find($id);
+        if($model==null){
+            return response()->json(["L'id ne correspond à aucune information"],404);// fails verifier s'il ya echec
+        }
+        return response()->json($model);
     }
 
     /**
@@ -48,7 +73,31 @@ class Usercontroller extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data=array();
+        $model=new User();
+        $fillable=$model->getFillable();//récupérer les champs modifiables
+        $requests=$request->all();//récupérer tous les champs envoyés
+        foreach ($requests as $key => $value) {
+            if(in_array($key,$fillable)){
+                $data[$key]=$value;
+            }
+        }
+        $validation=Validator::make($data,[
+            'email'=>'email',
+            'first_name'=>'min:1',
+            'last_name'=>'min:1',
+            'password'=>'min:8',
+            'type'=>[Rule::in(['agent','customer'])]//rule definie la regle
+        ]);
+        if($validation->fails()){
+            return response()->json($validation->getMessageBag(),422);// fails verifier s'il ya echec
+        }
+        $model=User::find($id);
+        if($model==null){
+            return response()->json(["L'id ne correspond à aucune information"],404);// fails verifier s'il ya echec
+        }
+        $model->update($data);
+        return response()->json($model);
     }
 
     /**
@@ -59,6 +108,11 @@ class Usercontroller extends Controller
      */
     public function destroy($id)
     {
-        //
+        $model=User::find($id);
+        if($model==null){
+            return response()->json(["L'id ne correspond à aucune information"],404);// fails verifier s'il ya echec
+        }
+        $model->delete();
+        return response()->json($model);
     }
 }
